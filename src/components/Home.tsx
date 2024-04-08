7427aa2fea13eea8b06a017f20fcd4759926cced
Júlia Sendra <juliasendra98@gmail.com>
2024-04-08 21:00:01 +0200

    Create Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,17 @@
+import "../App.css";
+
 function Home() {
-  return <p>Home</p>;
+  return (
+    <div className="header">
+      <div className="quote">
+        <h1>
+          <p>Hey there! I'm Júlia  &#x1F44B;</p>
+          <p>A full-stack developer</p>
+          <p>Building seamless digital experiences</p>
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
