10bf6122f99005b0c5d2ca333248bf6c1c17d30a
Júlia Sendra <juliasendra98@gmail.com>
2024-04-09 00:46:53 +0200

    Update Skills with devicon

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,12 +3,176 @@ import "../App.css";
 function Skills() {
   return (
     <div className="skills-section">
-    <div>
-    <h4>SKILLS</h4>
-    <h2>Technologies, frameworks, languages and more...</h2>
-    
-</div>
-</div>
+      <div>
+        <h4>SKILLS</h4>
+        <h2>Technologies, frameworks, languages and more...</h2>
+        <div className="container-icons">
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-html5-plain"></i>
+            <p>HTML5</p>
+          </div>
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-css3-plain"></i>
+            <p>CSS3</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-javascript-plain"></i>
+            <p>JavaScript ES6</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-react-original"></i>
+            <p>React.js</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-git-plain"></i>
+            <p>git</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-github-original"></i>
+            <p>GitHub</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-vscode-plain"></i>
+            <p>VSCode</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-nodejs-plain-wordmark"></i>
+            <p>node.js</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-npm-original-wordmark"></i>
+            <p>npm</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-mongodb-plain"></i>
+            <p>mongoDB</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-mongoose-original"></i>
+            <p>mongoose</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-express-original"></i>
+            <p>express.js</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-typescript-plain"></i>
+            <p>TypeScript</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-postman-plain"></i>
+            <p>Postman</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-netlify-plain"></i>
+            <p>Netlify</p>
+          </div>
+
+          <div className="box-icon">
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+            />
+            <i className="devicon-nodemon-plain"></i>
+            <p>nodemon</p>
+          </div>
+
+
+
+
+
+        </div>
+      </div>
+    </div>
   );
 }
 
