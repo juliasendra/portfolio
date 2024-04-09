cb69a638519261307140d1db8c3add18dfd9e50e
Júlia Sendra <juliasendra98@gmail.com>
2024-04-09 23:58:08 +0200

    Improve visual aspect of About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,27 @@ import "../App.css";
 import profilepic from "../assets/images/profilepic.png";
 function About() {
   return (
-   <div className="about-container">
-<div>
-    <h4>ABOUT</h4>
-    <h2>Who is Júlia?</h2>
-    <img src={profilepic} alt="Júlia Sendra's Profile Picture" className="profile-pic"/>
-    <h3>A Full-Stack Developer with a background in communication</h3>
-    <p>After a few years in the world of communication, I have decided to face a new challenge and make the leap into the ‘tech’ world, since it has always generated a lot of interest in me 🚀</p>
-</div>
-   </div>
+    <div className="about-container">
+      <div>
+        <h4>ABOUT</h4>
+        <h2>Who is Júlia?</h2>
+        <div className="about-description">
+          <img
+            src={profilepic}
+            alt="Júlia Sendra's Profile Picture"
+            className="profile-pic"
+          />
+          <div className="about-text">
+            <h3>A Full-Stack Developer with a background in communication</h3>
+            <p>
+              After a few years in the world of communication, I have decided to
+              face a new challenge and make the leap into the ‘tech’ world,
+              since it has always generated a lot of interest in me 🚀
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
